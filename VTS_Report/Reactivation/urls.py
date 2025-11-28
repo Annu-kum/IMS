d99faebbae4bc73b53a9779d0de7a959c7aa395b
Annu-kum <annuk5523@gmail.com>
2025-11-28 12:15:59 +0530

    removed unwanted apis from folders

--- VTS_Report/Reactivation/urls.py
+++ VTS_Report/Reactivation/urls.py
@@ -15,15 +15,6 @@ urlpatterns = [
     path('updateReactivate/<int:id>/',views.updateReactivateviewsets.as_view(),name='updatereactive'),
     path('update-reactivation/<int:id>/', views.UpdatereactivateLetterHeadViewSets.as_view(), name='update-reactivation'),
     path('get_file_url/<int:id>/', views.get_file_url, name='get_file_url'), 
-    path('total/count/', views.ReactivationCountView.as_view(), name='Reactivation-count'),
-    path('total/new-count/', views.NewReactiveCountView.as_view(), name='Reactivation-new-count'),
-    path('total/renewal-count/',views.RenewalReactivationCountView.as_view(),name='Reactivation-renewal-count'),
-    path('date/today-count/', views.TodayReactiveCountView.as_view(), name='Reactivation-today-count'),
-    path('today-new-count/', views.TodayNewInstallCountView.as_view(), name='Reactivation-today-new-count'),
-    path('today-renewal-count/', views.TodayRenewalReactiveCountView.as_view(), name='Reactivation-today-renewal-count'),
-    path('yesterday-count/', views.YesterdayReactiveCountView.as_view(), name='Reactivation-yesterday-count'),
-    path('yesterday-new-count/', views.YesterdayNewReactivateCountView.as_view(), name='Reactivation-yesterday-new-count'),
-    path('yesterday-renewal-count/',views.YesterdayRenewalReactivationCountView.as_view(),name='Reactivation-yesterday-renewal-count'),
     path('import',views.BulkImportView.as_view(),name='bulkupload'),
     path('otrReactivationReport/',views.OTRReactivationReportView.as_view(),name='otrReactivation_report')
 ]
